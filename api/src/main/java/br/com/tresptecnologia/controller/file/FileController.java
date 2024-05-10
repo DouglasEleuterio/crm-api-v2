0ae2341ed4507f35c254136520f5cb8750e0692e
Douglas Eleutério <douglas.versato@gmail.com>
2024-05-09 22:59:31 -0300

    Controller de testes para conversão de arquivo xml local para entidade

--- api/src/main/java/br/com/tresptecnologia/controller/file/FileController.java
+++ api/src/main/java/br/com/tresptecnologia/controller/file/FileController.java
@@ -2,9 +2,13 @@ package br.com.tresptecnologia.controller.file;
 
 import br.com.tresptecnologia.core.exception.DomainException;
 import br.com.tresptecnologia.model.upload.AnexoUploadResponse;
+import br.com.tresptecnologia.service.ParseService;
 import br.com.tresptecnologia.service.upload.IUploadService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +24,11 @@ import java.util.List;
 public class FileController {
 
     private final IUploadService uploadService;
+    private final ParseService parseService;
 
-    public FileController(IUploadService uploadService) {
+    public FileController(IUploadService uploadService, ParseService parseService) {
         this.uploadService = uploadService;
+        this.parseService = parseService;
     }
 
     @PostMapping
@@ -36,4 +42,11 @@ public class FileController {
     public List<AnexoUploadResponse> uploadList(@RequestParam("files") List<MultipartFile> file) throws DomainException {
         return uploadService.upload(file);
     }
+
+    @GetMapping("/parse/{arquivo-name}")
+    @Operation(description = "Converte um arquivo XML em Objeto")
+    public ResponseEntity<Object> parse(@PathVariable(name = "arquivo-name") String arquivoName) throws DomainException {
+        return ResponseEntity.ok(parseService.convertXmlToOject(arquivoName));
+    }
+
 }
