91e3b6ccb5362e582935f19561fcb02956f4b1ec
Douglas Eleutério <douglas.versato@gmail.com>
2024-05-09 22:56:40 -0300

    Entidades

---
+++ domain/src/main/java/br/com/tresptecnologia/entity/notafiscal/Det.java
@@ -0,0 +1,46 @@
+package br.com.tresptecnologia.entity.notafiscal;
+
+import br.com.tresptecnologia.core.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "DET_DET")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Det extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_DET")
+    @SequenceGenerator(name = "SQ_DET", sequenceName = "SQ_DET", allocationSize = 1)
+    @Column(name = "DET_ID", nullable = false)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "INF_ID", foreignKey = @ForeignKey(name = "FK_INF_DET"))
+    private InfNFe infNFe;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "PRD_ID", foreignKey = @ForeignKey(name = "FK_PRD_DET"))
+    public Prod prod ;
+}
