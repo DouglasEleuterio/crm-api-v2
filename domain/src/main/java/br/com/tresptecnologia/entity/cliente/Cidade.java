7759958e635cde1505f3245e965d76d5adfb5bc9
Douglas Eleuterio <douglas.versato@gmail.com>
2024-06-05 22:09:56 -0300

    Adição de migration e entidade de Cidade

--- domain/src/main/java/br/com/tresptecnologia/entity/cliente/Cidade.java
+++ domain/src/main/java/br/com/tresptecnologia/entity/cliente/Cidade.java
@@ -1,13 +1,40 @@
 package br.com.tresptecnologia.entity.cliente;
 
+import br.com.tresptecnologia.core.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
-public class Cidade {
+@Getter
+@Setter
+@Entity
+@Table(name = "cidade")
+public class Cidade extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_CIDADE")
+    @SequenceGenerator(name = "SQ_CIDADE", sequenceName = "SQ_CIDADE", allocationSize = 1)
+    @Column(name = "id", nullable = false, length = 36)
+    private Long id;
 
+    @Column(name = "nome", nullable = false)
     private String nome;
+    @ManyToOne
+    @JoinColumn(name = "estado_id")
     private Estado estado;
+    @Column(name = "codigo_ibge")
     private String codigoIBGE;
 }
---
+++ domain/src/main/resources/db/migration/changes/20240605-2-add-cidade.sql
@@ -0,0 +1,18 @@
+--liquibase formatted sql
+--changeset douglas.ferreira:add-cidade
+
+CREATE SEQUENCE sq_cidade START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE cidade
+(
+    id               NUMBER(38, 0) NOT NULL,
+    data_criacao     TIMESTAMP     NOT NULL,
+    data_atualizacao TIMESTAMP     NOT NULL,
+    nome             VARCHAR2(255) NOT NULL,
+    estado_id        NUMBER(38, 0),
+    codigo_ibge      VARCHAR2(255),
+    CONSTRAINT pk_cidade PRIMARY KEY (id)
+);
+
+ALTER TABLE cidade
+    ADD CONSTRAINT FK_CIDADE_ON_ESTADO FOREIGN KEY (estado_id) REFERENCES estado (id);
