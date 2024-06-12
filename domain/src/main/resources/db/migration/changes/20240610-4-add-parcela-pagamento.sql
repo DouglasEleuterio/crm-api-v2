23c3997733049be8937e51db9b345eb58ed916d3
Douglas Eleutério <douglas.versato@gmail.com>
2024-06-12 19:25:42 -0300

    Ajustes em tabelas

--- domain/src/main/resources/db/migration/changes/20240610-4-add-parcela-pagamento.sql
+++ domain/src/main/resources/db/migration/changes/20240610-4-add-parcela-pagamento.sql
@@ -13,7 +13,9 @@ CREATE TABLE parcela_pagamento
     numero_parcela   INTEGER       NOT NULL,
     pagamento_id     NUMBER(38, 0),
     data_atualizacao TIMESTAMP     NOT NULL,
+    data_criacao     TIMESTAMP     NOT NULL,
     dataCriacao      TIMESTAMP     NOT NULL,
+    situacao         NUMBER(1)     NOT NULL,
     CONSTRAINT pk_parcela_pagamento PRIMARY KEY (id)
 );
 
